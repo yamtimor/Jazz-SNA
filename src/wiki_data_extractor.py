dde564803a739eea5f4a364cf8d2852b0c08bba0
yamtimor <yamtimor@gmail.com>
2022-11-24 15:43:49 +0200

    change the way of players extraction

--- src/wiki_data_extractor.py
+++ src/wiki_data_extractor.py
@@ -1,16 +1,20 @@
 import wikipedia
-import pandas
 from pprint import pprint
 
 # list of bebop jazz players
 bebop_players = wikipedia.page("List of bebop musicians").content
 
-def wiki_data_extractor():
+def extract_players():
     players = []
-    for element in filter(lambda x: bool(x) and not x.startswith("=="), bebop_players.split('\n')):
-        players.append(element.split('-')[0][:-1])
 
+    for element in filter(lambda x: bool(x) and not x.startswith("=="), bebop_players.split("\n")[1:]):
+        players.append(
+            {
+                "player": element.split('-')[0][:-1],
+             "instrument": element.split('-')[0][:-1]
+            }
+        )
     return players
 
 if __name__ == "__main__":
-    print(wiki_data_extractor())
+    print(extract_players())
