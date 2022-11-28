8438ae1f424fb182920a3ce4928a28919d61bddf
yamtimor <yamtimor@gmail.com>
2022-11-28 15:47:23 +0200

    add Player class to the code

--- src/wiki_data_extractor.py
+++ src/wiki_data_extractor.py
@@ -10,11 +10,24 @@ def extract_players():
     for element in filter(lambda x: bool(x) and not x.startswith("=="), bebop_players.split("\n")[1:]):
         players.append(
             {
-                "player": element.split('-')[0][:-1],
-             "instrument": element.split('-')[0][:-1]
+                "player": element.split("-")[0][:-1],
+             "instrument": element.split("-")[0][:-1]
             }
         )
     return players
 
+class Player:
+
+    def __init__(self, name, instrument, discography, sideman):
+        self.name = name
+        self.instrument = instrument
+        self.discography = discography
+        self.sideman = sideman
+
+    # temporary:
+    def __str__(self):
+        return f"info about {self.name}"
+
 if __name__ == "__main__":
-    print(extract_players())
+    player_test = Player('John Coltrane', 'Saxophone', [1,2,3], [4,5])
+    print(player_test)
