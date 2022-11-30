fe27e5ca7c12fb195d651fc9bca6a10d1fcea93c
yamtimor <yamtimor@gmail.com>
2022-12-01 01:57:14 +0200

    create a function that extract the discography of each player

--- src/main.py
+++ src/main.py
@@ -1,15 +1,15 @@
 import wikipedia
 from pprint import pprint
 from player import Player
-from wiki_data_extractor import extract_players
+from wiki_data_extractor import extract_players, extract_discography
 
 
 def main():
     players_list = extract_players()
-    for player in players_list:
-        p = Player(player['player'], player['instrument'], 'test', 'test')
-        print(p)
-
+    # for player in players_list:
+        # p = Player(player['player'], player['instrument'], 'test', 'test')
+        # print(p)
+    pprint(extract_discography(players_list[0]['player']))
 
 if __name__ == "__main__":
     main()
--- src/wiki_data_extractor.py
+++ src/wiki_data_extractor.py
@@ -6,6 +6,7 @@ from player import Player
 bebop_players = wikipedia.page("List of bebop musicians").content
 
 def extract_players():
+
     players = []
 
     for element in filter(lambda x: bool(x) and not x.startswith("=="), bebop_players.split("\n")[1:]):
@@ -16,4 +17,19 @@ def extract_players():
             }
         )
     return players
+
+def extract_discography(player):
+
+    page = wikipedia.page(player).content
+    discography_with_titles = list(filter(lambda x: bool(x), page.split("== Discography ==")[1].split("\n")))
+    discography_with_titles = discography_with_titles[:discography_with_titles.index("== References ==")]
+
+    leader = discography_with_titles[:discography_with_titles.index("=== As sideman ===")][1:]
+    sideman = discography_with_titles[discography_with_titles.index("=== As sideman ===")+1:]
+
+    discography = {
+        "leader": leader,
+        "sideman": sideman
+     }
+    return discography
 
